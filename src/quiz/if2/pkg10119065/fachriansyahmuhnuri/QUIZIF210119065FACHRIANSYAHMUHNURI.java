79980076367401e27c3436ad9b7ad995f7e65d9a
fachriansyahmni <fachriansyahmni@gmail.com>
2020-11-27 20:30:48 +0700

    upload

---
+++ src/quiz/if2/pkg10119065/fachriansyahmuhnuri/QUIZIF210119065FACHRIANSYAHMUHNURI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if2.pkg10119065.fachriansyahmuhnuri;
+
+/**
+ *
+ * @author Fachriansyah PC
+ */
+public class QUIZIF210119065FACHRIANSYAHMUHNURI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/quiz/if2/pkg10119065/fachriansyahmuhnuri/ServiceItem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if2.pkg10119065.fachriansyahmuhnuri;
+
+/**
+ *
+ * @author Fachriansyah PC
+ */
+public interface ServiceItem {
+    public void displayService();
+    public float getPrice();
+    public boolean checkMemberStatus(String statusMember);
+    public float getSale(boolean isMember, float parServicePrice);
+}
